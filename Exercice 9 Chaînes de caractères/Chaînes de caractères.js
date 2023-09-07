3e4a0ad773985415dd15a63fb43747cdb78bde22
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 10:43:07 +0200

    exercice chaine de caractère

---
+++ Exercice 9 Chaînes de caractères/Chaînes de caractères.js
@@ -0,0 +1,5 @@
+let nomComplet = "Freyss"; // Remplacez "Votre Nom Complet" par votre nom complet
+let longueurNom = nomComplet.length;
+
+console.log("Nom Complet :", nomComplet);
+console.log("Longueur de la chaîne :", longueurNom);
