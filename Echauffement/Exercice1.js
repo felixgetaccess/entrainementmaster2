c95ef9bfc3272c86ccb6c30a165a44de6b2ba97d
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 10:09:19 +0200

    exercice 1 test

---
+++ Echauffement/Exercice1.js
@@ -0,0 +1,4 @@
+let nom = "VotreNom"; // Remplacez "VotreNom" par le nom que vous souhaitez attribuer
+let âge = 25; // Remplacez 25 par l'âge que vous souhaitez attribuer
+console.log("Nom :", nom);
+console.log("Âge :", âge);
