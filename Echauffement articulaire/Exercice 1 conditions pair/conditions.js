8bc38ca067e6c59a87da9a0718ed8ab481468ec5
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 11:40:52 +0200

    push exercice 3 et 4

--- Echauffement articulaire/Exercice 1 conditions pair/conditions.js
+++ Echauffement articulaire/Exercice 1 conditions pair/conditions.js
@@ -1,14 +1,13 @@
 function verifierPairImpair(nombre) {
     if (nombre % 2 === 0) {
-        return "Le nombre est pair.";
+        console.log(`Le nombre ${nombre} est pair.`);
     } else {
-        return "Le nombre est impair.";
+        console.log(`Le nombre ${nombre} est impair.`);
     }
 }
+
+verifierPairImpair(7);
 
 let resultat = verifierPairImpair(7);
 let resultat2 = verifierPairImpair(8);
 let resultat1 = verifierPairImpair(32213);
-console.log(resultat);
-console.log(resultat2);
-console.log(resultat1);
