282e6f3a41f93c592e91fc15d7ccadc14afc5d1c
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 10:13:15 +0200

    push exercice 2

---
+++ Exercice 2/Exercice1.js
@@ -0,0 +1,12 @@
+let nombre1 = 10;
+let nombre2 = 5;
+
+let somme = nombre1 + nombre2;
+let différence = nombre1 - nombre2;
+let produit = nombre1 * nombre2;
+let quotient = nombre1 / nombre2;
+
+console.log("Somme :", somme);
+console.log("Différence :", différence);
+console.log("Produit :", produit);
+console.log("Quotient :", quotient);
