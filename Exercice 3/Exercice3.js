ce0349011569dac23428e39dc01dac78d84bba90
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 10:17:12 +0200

    test pour l'echec

--- Exercice 3/Exercice3.js
+++ Exercice 3/Exercice3.js
@@ -1,4 +1,4 @@
-let note = 65; // Remplacez 65 par la note que vous souhaitez attribuer
+let note = 49; // Remplacez 65 par la note que vous souhaitez attribuer
 
 if (note >= 50) {
     console.log("Réussite");
