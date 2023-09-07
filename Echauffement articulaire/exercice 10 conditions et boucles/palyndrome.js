ccdbca2f4a57d57f987b9d1a05aca06439614030
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 11:59:43 +0200

    push palyndrome

---
+++ Echauffement articulaire/exercice 10 conditions et boucles/palyndrome.js
@@ -0,0 +1,15 @@
+function estPalindrome(chaine) {
+    // Retirer les espaces et mettre tout en minuscules
+    chaine = chaine.replace(/\s/g, "").toLowerCase();
+
+    // Inverser la chaîne
+    let chaineInverse = chaine.split("").reverse().join("");
+
+    // Vérifier si la chaîne originale est égale à la chaîne inversée
+    return chaine === chaineInverse;
+}
+
+let chaine = "Monter en haut";
+let estUnPalindrome = estPalindrome(chaine);
+
+console.log(`La phrase "${chaine}" est un palindrome : ${estUnPalindrome}`);
