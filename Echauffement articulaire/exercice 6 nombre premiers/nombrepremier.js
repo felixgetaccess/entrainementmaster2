630f3d4a9616cb03c071ddb260154c215fa20c94
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 11:51:40 +0200

    push

--- Echauffement articulaire/exercice 6 nombre premiers/nombrepremier.js
+++ Echauffement articulaire/exercice 6 nombre premiers/nombrepremier.js
@@ -1,17 +1,7 @@
-function estNombrePremier(nombre) {
-    if (nombre < 2) {
-        return false;
+for (let i = 1; i <= 5; i++) {
+    let ligne = "";
+    for (let j = 1; j <= i; j++) {
+        ligne += "*";
     }
-    for (let i = 2; i < nombre; i++) {
-        if (nombre % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-for (let i = 1; i <= 50; i++) {
-    if (estNombrePremier(i)) {
-        console.log(i);
-    }
+    console.log(ligne);
 }
