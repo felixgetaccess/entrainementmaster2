fc31620d568d42a307d3b486ec697ab6ff890f78
Felix Freyss <felix.freyss@gmail.com>
2023-09-07 11:24:00 +0200

    Push exercice 8 livre

---
+++ Mobilisation Articulaire/Exercice 8 objet livre/livre.js
@@ -0,0 +1,8 @@
+let livre = {
+    titre: "Le Seigneur des Anneaux",
+    auteur: "J.R.R. Tolkien",
+    anneePublication: 1954
+};
+console.log("Titre du livre :", livre.titre);
+console.log("Auteur du livre :", livre.auteur);
+console.log("Date de publication :", livre.anneePublication);
